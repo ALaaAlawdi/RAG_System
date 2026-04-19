1ad0e1377614b048b0fa28170ac41166e5a2509c
ALaaAlawdi <AlaaAlawdi01@gmail.com>
2026-04-19 21:26:53 +0300

    add Field validation pydantic

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,15 @@
 import os
 from pydantic_settings import BaseSettings
-
+from pydantic import Field
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str
+    OPENAI_API_KEY: str = Field(..., env="OPENAI_API_KEY")
 
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
+
 os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
+
